4d8aa9e1cddeb8e35bff498b32e4b5947f507cdf
Harsh <harshbaba007@gmail.com>
2019-09-09 21:14:36 +0530

    Fix By Harsh

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -11,39 +11,15 @@ export class DashboardComponent implements OnInit {
   public UpcomingEvents = [];
    constructor(private myservice: AlleventsService) {}
    ngOnInit() {
-      this.myservice.getData().subscribe((data) => {
-         this.UpcomingEvents = Array.from(Object.keys(data), k=>data[k]);
-         console.log(this.UpcomingEvents[1]);
+      this.myservice.getData().subscribe((data:any) => {
+         //this.UpcomingEvents = Array.from(Object.keys(data), k=>data[k]);
+         //console.log(this.UpcomingEvents[1]);
+         console.log(data);
+         if(data.success){
+            this.UpcomingEvents = data.data;
+         }else{
+           console.log('Something went wrong');
+         }
       });
    }
-  // UpcomingEvents = [
-  //   {
-  //    "id":"",
-  //    "eventType":"Birthday",
-  //    "eventName":"Rahul's Birthday",
-  //    "eventDate":"12-Jul-2019",
-  //    "name":"harsh",
-  //    "description":"lorem ipsum",
-  //    "eventCreatedBy":"abc@com",
-  //   },
-  //   {
-  //    "id":"",
-  //    "eventType":"Aniversary",
-  //    "eventName":"Rahul's Anniversary",
-  //    "eventDate":"12-Jul-2019",
-  //    "name":"harsh",
-  //    "description":"lorem ipsum",
-  //    "eventCreatedBy":"abc@com",
-  //   },
-  //   {
-  //    "id":"",
-  //    "eventType":"One Time Event",
-  //    "eventName":"5km walk event",
-  //    "eventDate":"12-Jul-2019",
-  //    "name":"harsh",
-  //    "description":"lorem ipsum",
-  //    "eventCreatedBy":"abc@com",
-  //   }
-  //   ];
-
 }
